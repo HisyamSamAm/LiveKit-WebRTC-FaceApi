9fdb7776cfa308110e65d4c29c6306f6ab93c666
HisyamSamAm <mhisyamnajwan@gmail.com>
2025-06-25 12:59:39 +0700

    Bug Build

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
     formats: ['image/webp'],
-    unoptimized: true,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     // Important: return the modified config
